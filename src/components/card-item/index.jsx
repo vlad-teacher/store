4f3ad18d1b04e88b9ae3f0b798d261ff552affca
Vlad <vlad.it.instructor@gmail.com>
2024-04-24 22:58:33 +0300

    card items

--- src/components/card-item/index.jsx
+++ src/components/card-item/index.jsx
@@ -1,21 +1,25 @@
 import React from "react";
-import heartImg from '../../assets/heart.svg';
-import cartImg from '../../assets/cart.svg';
-import styles from './index.module.scss';
+import heartImg from "../../assets/heart.svg";
+import cartImg from "../../assets/cart.svg";
+import styles from "./index.module.scss";
+import { BASE_URL } from "../../constants";
 
-// sale, description, image, discount
-export const CardItem = () => {
-    return <div className={styles.wrapper}>
-        <div className={styles.header}>
-            <div className={styles.discount}>50%</div>
-            <img/>
-            <img src={heartImg} className={styles.heart}/>
-            <img src={cartImg} className={styles.cart}/>
-        </div>
-        <div className={styles.description}>Description</div>
+export const CardItem = ({ price, description, image, discont, discontPercent }) => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.header}>
+        <div className={styles.discount}>{discontPercent}%</div>
+        <img src={BASE_URL + image} alt="card" className={styles.cardImage} />
+        <img src={heartImg} className={styles.heart} alt="heart" />
+        <img src={cartImg} className={styles.cart} alt="cart" />
+      </div>
+      <div className={styles.info}>
+        <div className={styles.description}>{description}</div>
         <div className={styles.prices}>
-            <div className={styles.newPrice}>500</div>
-            <div className={styles.oldPrice}>1000</div>
+          <div className={styles.newPrice}>{discont}</div>
+          <div className={styles.oldPrice}>{price}</div>
         </div>
+      </div>
     </div>
+  );
 };
--- src/views/main/index.jsx
+++ src/views/main/index.jsx
@@ -1,7 +1,11 @@
-import React, { useEffect } from "react";
-import {BASE_URL} from '../../constants';
+import React, { useEffect, useState } from "react";
+import { BASE_URL } from "../../constants";
+import { CardItem } from "../../components/card-item";
+import styles from "./index.module.scss";
+import { getDiscountPercent } from "../../utils/getDiscountPercent";
 
 export const Main = () => {
+  const [saleItems, setSaleItems] = useState([]);
 
   useEffect(() => {
     fetch(`${BASE_URL}/products/all`)
@@ -9,14 +13,35 @@ export const Main = () => {
         return res.json();
       })
       .then((data) => {
-        const saleItems = data.filter(({ discont_price }) => discont_price !== null);
-
+        setSaleItems(
+          data
+            .sort(
+              (elem, elem2) =>
+                getDiscountPercent(elem2.price, elem2.discont_price) -
+                getDiscountPercent(elem.price, elem.discont_price)
+            )
+            .filter(
+              ({ discont_price }, index) => discont_price !== null && index < 4
+            )
+        );
       });
   }, []);
 
   return (
     <>
       <h1>Main</h1>
+      <div className={styles.sales}>
+        {saleItems.map(({ price, discont_price, description, image, id }) => (
+          <CardItem
+            key={id}
+            price={price}
+            discont={discont_price}
+            discontPercent={getDiscountPercent(price, discont_price)}
+            description={description}
+            image={image}
+          />
+        ))}
+      </div>
     </>
   );
 };
